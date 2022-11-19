b1244b775988abe6ba3889931306eb97d498a3e0
visheshnarain <117995541+visheshnarain@users.noreply.github.com>
2022-11-19 20:40:37 +0530

    Add files via upload

---
+++ numbers in range.py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+for number in list1:
+    if number>0:
+        print (number)
+
+for num in list2:
+    if num>0:
+        print (num)
+    
+    
+       
