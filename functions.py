3e1f3a51bba1c80d2ac3f75412e953e04e370f87
visheshnarain <117995541+visheshnarain@users.noreply.github.com>
2022-11-28 23:33:54 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,9 @@
+def most_frequent(string):
+    d={}
+    for key in string:
+        if key not in d:
+            d[key]=1
+        else:
+            d[key] +=1
+    return d
+print (most_frequent('mississippi'))
